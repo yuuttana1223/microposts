9398ccde32be9435304d1af979f726d3de59f4a6
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2020-12-31 00:37:40 +0900

    repair error microposts nil

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
